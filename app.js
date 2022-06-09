457ff44203b1ebd0986440a04a94c10cc807346a
shanto <hhassibul@gmail.com>
2022-06-09 17:22:15 +0600

    ;frdoij

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-// function pp(){
+function pp(){
   
-//   phoneQuantity = parseFloat(document.getElementById('iphn-q').value);
-//   phonePrice = document.getElementById('price').innerText;
-//   phonPrice = parseFloat(phonePrice)
-//   console.log(phonePrice);
-//   console.log(phoneQuantity);
-//   document.getElementById('price').innerHTML = 1219 * phoneQuantity
-// }
+  phoneQuantity = parseFloat(document.getElementById('iphn-q').value);
+  document.getElementById('iphn-q').value = phoneQuantity +1
+  newQuantity = document.getElementById('iphn-q').value
+  phonePrice = document.getElementById('price').innerText;
+  phonPrice = parseFloat(phonePrice)
+  console.log(phonePrice);
+  console.log(phoneQuantity);
+  document.getElementById('price').innerHTML = 1219 * newQuantity
+}
 document.getElementById('case-plus').addEventListener('click', function(){
-  console.log('dsi');
+  caseNumber =parseFloat( document.getElementById('case-number').value);
+  document.getElementById('case-number').value = caseNumber + 1;
+  caseNum = parseFloat( document.getElementById('case-number').value);
+  document.getElementById('case-price').innerHTML = caseNum * 59 ;
+  subTotal = parseFloat(document.getElementById('sub-total').innerHTML);
+  document.getElementById('sub-total').innerHTML = subTotal + 59
+  if(document.getElementById("case-number").value >= 1 ){
+    document.getElementById("case-minus").disabled = false;
+  }
+
+})
+document.getElementById('case-minus').addEventListener('click', function(){
+  caseMinus = parseFloat(document.getElementById("case-number").value);
+  
+  document.getElementById("case-number").value = caseMinus - 1;
+  if(document.getElementById("case-number").value == 0 ){
+    document.getElementById("case-minus").disabled = true;
+    subTotal = parseFloat(document.getElementById('sub-total').innerHTML);
+    document.getElementById('sub-total').innerHTML = subTotal - 59
+  }
+
 })
