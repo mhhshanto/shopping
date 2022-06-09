7f2f712b8588d99c01310d0929aa1ba812041ca9
shanto <hhassibul@gmail.com>
2022-06-09 13:03:11 +0600

    ukdyf

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-document.getElementById('iphn-plus').addEventListener('click',function(){
-  phoneQuantity = document.getElementById('iphn-q').value;
-  phonePrice = document.getElementById('')
+// function pp(){
+  
+//   phoneQuantity = parseFloat(document.getElementById('iphn-q').value);
+//   phonePrice = document.getElementById('price').innerText;
+//   phonPrice = parseFloat(phonePrice)
+//   console.log(phonePrice);
+//   console.log(phoneQuantity);
+//   document.getElementById('price').innerHTML = 1219 * phoneQuantity
+// }
+document.getElementById('case-plus').addEventListener('click', function(){
+  console.log('dsi');
 })
