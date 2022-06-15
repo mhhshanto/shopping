f4a0fa8767a87cd0db514a482b4d246947f3de20
shanto <hhassibul@gmail.com>
2022-06-16 00:13:49 +0600

    sdt

--- newApp.js
+++ newApp.js
@@ -1,6 +1,6 @@
 // case
-function updateCaseNumber(isIncreasing){
-  const caseInput = document.getElementById('case-number')
+function updateProductNumber(rate,product,isIncreasing){
+  const caseInput = document.getElementById(product+'-number')
   let caseNumber = caseInput.value;
   if(isIncreasing){
     caseNumber = parseFloat(caseNumber) + 1;
@@ -10,38 +10,44 @@ function updateCaseNumber(isIncreasing){
     caseNumber = parseFloat(caseNumber) - 1;
   }
   caseInput.value = caseNumber;
-  const caseTotal = document.getElementById('case-price')
-  caseTotal.innerHTML = caseNumber * 59;
+  const caseTotal = document.getElementById(product+'-total')
+  caseTotal.innerHTML = caseNumber * rate;
 }
-
+// handle case increase decrease
 
 document.getElementById('case-plus').addEventListener('click',function(){
-  updateCaseNumber(true)
+  updateProductNumber(59,'case',true)
  
 })
 document.getElementById('case-minus').addEventListener('click',function(){
- updateCaseNumber()
+ updateProductNumber(59,'case',false)
 })
-//iphone
-function updateIphone(increasing){
-  const iphoneInput = document.getElementById('phone-number');
-  let iphoneNumber = iphoneInput.value
-  if(increasing){
-    iphoneNumber = parseInt(iphoneNumber) +1;
-  }else if(iphoneNumber > 0){
-    iphoneNumber = parseInt(iphoneNumber) -1;
-  }
-  iphoneInput.value = iphoneNumber;
-  const phoneTotal = document.getElementById('price')
-  phoneTotal.innerHTML = iphoneNumber * 1219;
-}
 
-document.getElementById('phone-minus').addEventListener('click',function(){
- updateIphone()
- 
-})
+
+//iphone
+
+
+
+// function updateIphone(increasing){
+//   const iphoneInput = document.getElementById('phone-number');
+//   let iphoneNumber = iphoneInput.value
+//   if(increasing){
+//     iphoneNumber = parseInt(iphoneNumber) +1;
+//   }else if(iphoneNumber > 0){
+//     iphoneNumber = parseInt(iphoneNumber) -1;
+//   }
+//   iphoneInput.value = iphoneNumber;
+//   const phoneTotal = document.getElementById('price')
+//   phoneTotal.innerHTML = iphoneNumber * 1219;
+// }
+
+
+// iphoneee increasee decreaseeeeeeeeeeeeee
+
 document.getElementById('phone-plus').addEventListener('click',function(){
-  updateIphone(true)
-  
+  updateProductNumber(1219,'phone',true)
 })
+document.getElementById('phone-minus').addEventListener('click',function(){
+  updateProductNumber(1219,'phone',false)
+ })
 
